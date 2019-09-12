ccd32a12254d7b8895e5111a298fa3c517d98815
sanjeet pal <sanjeetpal123456789@gmail.com>
2019-09-12 19:28:10 +0000

    second comment

--- movies_db/settings.py
+++ movies_db/settings.py
@@ -136,5 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sanjeetpal007@gmail.com'
-EMAIL_HOST_PASSWORD = 'viwekkumarpal'
+EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
